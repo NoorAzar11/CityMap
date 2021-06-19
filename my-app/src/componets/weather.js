1ac6bc720095d39b46f3e2588bae8059260eabec
NoorAzar11 <nooraaz945@gmail.com>
2021-06-19 23:28:13 +0300

    updated

--- my-app/src/componets/weather.js
+++ my-app/src/componets/weather.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 
-class Weather extends React.Component{
+class Weather extends React.Component {
 
 
 
-render(){
-    return (
-        <div>
-              <Card style={{ width: '18rem' }}>
-                  {/* <Card.Header>{this.props.data.lat} {this.props.data.lon}</Card.Header> */}
-              <Card.Body>
+    render() {
+        return (
+            <div>
+                {
+                    this.props.weatherInFoserver.map((item) => {
+                        return (
+                            <Card style={{ width: '18rem' }}>
+                                {/* <Card.Header>{this.props.data.lat} {this.props.data.lon}</Card.Header> */}
+                                <Card.Body>
 
-              <Card.Title>
-{this.props.date}
-{this.props.description}
+                                    <Card.Title>
+                                        Daily Weather : {this.date}
+                                        <Card.Text> Weather Description :{this.description}
+                                        </Card.Text>
 
-              </Card.Title>
+                                    </Card.Title>
 
-              </Card.Body>
-              </Card>
+                                </Card.Body>
+                            </Card>
+                        )
+                    })
+                }
         </div>
-    )
-}
+        )
+    }
 }
 
 export default Weather;
